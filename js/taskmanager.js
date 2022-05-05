4d19309e8840168dd2cb943a523554b3b2bbbe3d
L4L4Here <Malalalaie@gmail.com>
2022-05-05 11:11:13 -0700

    Update taskmanager.js

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -1,6 +1,33 @@
 
 class taskmanager {
-    constructor(name) {
+    constructor() {
         this.currentId;
       }
 }
+
+const task1 = {
+  id: 1,
+  name: 'Take out the trash',
+  description: 'Take out the trash to the front of the house',
+  assignedTo: 'Nick',
+  dueDate: '2020-09-20',
+  status: 'TODO'
+};
+
+const task2 = {
+  id: 2,
+  name: 'Cook Dinner',
+  description: 'Prepare a healthy serving of pancakes for the family tonight',
+  assignedTo: 'Nick',
+  dueDate: '2020-09-20',
+  status: 'TODO'
+};
+
+const task3 = {
+  id: 3, 
+  name: 'Chop vegetables',
+  description:  'Chop all vegetables needed for dinner',
+  
+}
+
+console.log(tasks)
